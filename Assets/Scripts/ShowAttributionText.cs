01bccc8970884e702c3f2c453daf88149a07136a
Travis <travisamador@gmail.com>
2022-09-21 17:52:00 -0400

    UI text stays on screen when project runs

--- Assets/Scripts/ShowAttributionText.cs
+++ Assets/Scripts/ShowAttributionText.cs
@@ -5,17 +5,21 @@ using UnityEngine.UI;
 
 public class ShowAttributionText : MonoBehaviour
 {
+    //creates textvalue in inspector
     public string textValue;
+    //holds a UI text element in inspector
     public Text textElement;
     // Start is called before the first frame update
     void Start()
     {
+        //sets text field in inspector
         textElement.text = textValue; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        textElement.text = textValue;
+        //value can be changed while game is running - not necessary
+        //textElement.text = textValue;
     }
 }
